ce4754bbd3f33260d23915bf10ff789a337187eb
Michael Alexander <michael.m.alexander@outlook.com>
2021-04-18 19:53:31 -0700

    Updates

---
+++ precache-manifest.bc9f33c4532cf7025985bf217096ab9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da575a426d489d29a082c94fc291a17d",
+    "url": "/react-portfolio-2/index.html"
+  },
+  {
+    "revision": "2cf914501994f9543a0b",
+    "url": "/react-portfolio-2/static/css/2.1c2618a6.chunk.css"
+  },
+  {
+    "revision": "2d6e26abe8a8f4702d48",
+    "url": "/react-portfolio-2/static/css/main.7b3e7df6.chunk.css"
+  },
+  {
+    "revision": "2cf914501994f9543a0b",
+    "url": "/react-portfolio-2/static/js/2.640eac4e.chunk.js"
+  },
+  {
+    "revision": "2d6e26abe8a8f4702d48",
+    "url": "/react-portfolio-2/static/js/main.a77dbf6f.chunk.js"
+  },
+  {
+    "revision": "75dfacc217c02238c5bd",
+    "url": "/react-portfolio-2/static/js/runtime~main.0d949698.js"
+  },
+  {
+    "revision": "e0af78a5901dca942f2b36c7796e7ec1",
+    "url": "/react-portfolio-2/static/media/Solar System.e0af78a5.png"
+  },
+  {
+    "revision": "f3d9eb0bb78a77eae1d67b4998e0df19",
+    "url": "/react-portfolio-2/static/media/cardio.f3d9eb0b.jpg"
+  },
+  {
+    "revision": "4dc6c453177f476c9f0abe611f596c23",
+    "url": "/react-portfolio-2/static/media/my pic.4dc6c453.png"
+  },
+  {
+    "revision": "668cbb873841abc8c15a443c7c8f1fbe",
+    "url": "/react-portfolio-2/static/media/random-book.668cbb87.jpg"
+  }
+]);
